b14427815fdef9cce04a8dc5164ac8bc43b0b51c
mcforsas <karolis.poviliunas@gmail.com>
2019-05-11 14:31:25 +0300

    Initiated project and created ScrollingListItem, which will be used in offer search screen.

---
+++ app/src/main/java/com/mcforsas/apps/molybdenum/Model/ListScrollingItem.java
@@ -0,0 +1,10 @@
+package com.mcforsas.apps.molybdenum.Model;
+
+/**
+ * Created by mcforsas on 19.5.11
+ * An object which displays post data. Is used for displaying offers in scrolling view. Will be used
+ * for displaying list of offers for the user, which will be clicked on and the directed to it's
+ * page for further info.
+ */
+public class ListScrollingItem {
+}
